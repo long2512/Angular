2a617e9c9906957fb212562e94e705f2088f0f9f
long2512 <buidanglong02@gmail.com>
2022-05-10 13:34:14 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent},
+  { path: 'product', component: ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
