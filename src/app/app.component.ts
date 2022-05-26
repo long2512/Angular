f0d8c18efeb9788028c631eb16583077f33228bb
long2512 <buidanglong02@gmail.com>
2022-05-26 12:08:23 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,24 +12,6 @@ export class AppComponent {
   onHandleAdd(product:any){
     this.productList.push(product)
   }
-  // myName="Hiệp"
-  // productList: IProduct[] = [
-  //   {
-  //     id: 1,
-  //     name: "product A",
-  //     price: 200,
-  //     status: false
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "product B",
-  //     price: 300,
-  //     status: true
-  //   }
-  // ]
-  // onHandleAdd(product:any){
-  //   console.log("product",product)
-  //   this.productList.push(product)
-  // }
+  
 
   }
